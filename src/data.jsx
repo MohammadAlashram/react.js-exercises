5dd65c51a7dc330515d0a9db7b2524af14335410
MohammadAlashram <71894852+MohammadAlashram@users.noreply.github.com>
2021-01-23 08:27:08 +0300

    First React Task
    First React Task

---
+++ src/data.jsx
@@ -0,0 +1,54 @@
+const data = [
+{
+  id: 1,
+  fullName: "Lara Mahfouz",
+  coverImg: "https://i.ibb.co/dQJCkFx/pixl1.jpg",
+  profileImg: "https://i.ibb.co/kGBvbF3/rsz-whatsapp-image-2021-01-23-at-55506-am.jpg",
+  facebook: "https://www.facebook.com/",
+  twitter: "https://www.linkedin.com/",
+  instagram: "https://github.com/",
+  description:"Designer | Design of Social Media Ads | Full Stack Web Developer | Front End & Back End |Trainee Coding Academy By Orange at Orange Jordan"
+},
+{
+  id: 2,
+  fullName: "Mohammad Alashram",
+  coverImg:"https://i.ibb.co/dQJCkFx/pixl1.jpg",
+  profileImg: "https://i.ibb.co/XY3NkGr/Whats-App-Image-2021-01-23-at-6-30-53-AM.jpg",
+  facebook: "https://www.facebook.com/",
+  twitter: "https://www.linkedin.com/",
+  instagram: "https://github.com/",
+  description:"Back-end | Front-end | Web Developer | PHP | HTML | CSS | JavaScript | Node JS | C# | React & React Native | Linux & Window (OS) | MySQL | SEO | Graphic Designer | Mobile Apps Developer | WordPress  "
+},
+{
+  id: 3,
+  fullName: "Aya Khwaldeh",
+  coverImg:"https://i.ibb.co/dQJCkFx/pixl1.jpg",
+  profileImg: "https://i.ibb.co/fxdGTJW/Whats-App-Image-2021-01-23-at-5-58-18-AM.jpg",
+  facebook: "https://www.facebook.com/",
+  twitter: "https://www.linkedin.com/",
+  instagram: "https://github.com/",
+  description:"Full Stack Developer Trainee at Orange Jordan | Back End Developer | Frontend Developer | Software Engineer | Developer | Computer engineer"
+},
+{
+  id: 4,
+  fullName: "Tala Al-Waheidi",
+  coverImg:"https://i.ibb.co/dQJCkFx/pixl1.jpg",
+  profileImg: "https://data.whicdn.com/images/326798445/original.jpg",
+  facebook: "https://www.facebook.com/",
+  twitter: "https://www.linkedin.com/",
+  instagram: "https://github.com/",
+  description:"Trainee in Orange Coding Academy | Backend Developer | Frontend Developer | Web Developer | Php MySQL | Laravel  "
+},
+{
+  id: 5,
+  fullName: "Mohammad Yacoub",
+  coverImg:"https://i.ibb.co/dQJCkFx/pixl1.jpg",
+  profileImg: "https://i.ibb.co/x2jb1FB/Whats-App-Image-2021-01-23-at-6-07-18-AM.jpg",
+  facebook: "https://www.facebook.com/",
+  twitter: "https://www.linkedin.com/",
+  instagram: "https://github.com/",
+  description:"Full Stack Web Developer | Trainee at Coding Academy by Orange | Engineer | Designer | Brand identity Developer |  "
+},
+];
+
+export default data;
